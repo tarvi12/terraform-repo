cc0b7c54848a999c4d3652749c95977bc2efe24d
tarvi12 <tarvikaur1994@gmail.com>
2025-12-18 12:39:51 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform{
     backend "s3" {
-    bucket         	   = "terrafrom-remote-tarvi"
+    bucket         	   = "terraform-remote-tarvi"
     key              	   = "state/terraform.tfstate"
     region         	   = "us-east-1"
   }
